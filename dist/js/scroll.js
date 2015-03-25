3efe9232b56a2446d668f13c2e441613dfda3f10
marionuevo <marionuevo@gmail.com>
2015-03-25 10:28:28 +0000

    Initial release
    Now supporting bower, gulp processing (CSS & HTML minifier)

---
+++ dist/js/scroll.js
@@ -0,0 +1,56 @@
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+			function( callback ){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
+
+(function(win, d) {
+
+	var $ = d.querySelector.bind(d);
+
+	var mainBG = $('.fixed-background');
+
+	var ticking = false;
+	var lastScrollY = 0;
+
+	function onResize () {
+		updateElements(win.pageYOffset);
+	}
+
+	function onScroll (evt) {
+
+		if(!ticking) {
+			ticking = true;
+			requestAnimFrame(updateElements);
+			lastScrollY = win.pageYOffset;
+		}
+	}
+
+	function updateElements () {
+
+		prefix(mainBG.style, "Transform", "translate3d(0," + 0 + 'px, 0)');
+		ticking = false;
+	}
+
+	function prefix(obj, prop, value) {
+		var prefs = ['webkit', 'Moz', 'o', 'ms'];
+		for (var pref in prefs) {
+			obj[prefs[pref] + prop] = value;
+		}
+	}
+
+	(function() {
+		updateElements();
+	})();
+
+	win.addEventListener('load', onResize, false);
+	win.addEventListener('resize', onResize, false);
+	win.addEventListener('scroll', onScroll, false);
+
+})(window, document);
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var minifyCSS = require('gulp-minify-css');
+var htmlmin = require('gulp-htmlmin');
+
+var tasks = ['scripts', 'index-minify', 'styles', 'bower-to-dist', 'images'];
+
+gulp.task('styles', function() {
+	gulp.src('src/css/*.css')
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts', function() {
+	gulp.src('src/js/*.js')
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('images', function() {
+	gulp.src('src/images/*')
+		.pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('index-minify', function() {
+	gulp.src('src/*.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(htmlmin({removeComments: true}))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('bower-to-dist', function() {
+	gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css'])
+		.pipe(gulp.dest('dist/css'));
+	gulp.src(['bower_components/bootstrap/dist/js/bootstrap.min.js'])
+		.pipe(gulp.dest('dist/js'));	
+	gulp.src(['bower_components/jquery/dist/jquery.min.js'])
+		.pipe(gulp.dest('dist/js'));
+	gulp.src(['bower_components/angularjs/angular.min.js'])
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', function (){
+	gulp.watch('src/css/*.css', tasks);
+});
+
+tasks.push('watch');
+
+gulp.task('default', tasks);
