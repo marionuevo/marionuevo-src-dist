620342dc52cdb2908801346291ec1ca79defd13b
marionuevo <marionuevo@gmail.com>
2015-03-29 22:28:11 +0100

    Finalized less to css processing and uncss

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,48 +1,54 @@
-var gulp = require('gulp');
-var minifyCSS = require('gulp-minify-css');
-var htmlmin = require('gulp-htmlmin');
-var uglify = require('gulp-uglify');
+var gulp = require('gulp'),
+	minifyCSS = require('gulp-minify-css'),
+	htmlmin = require('gulp-htmlmin'),
+	uglify = require('gulp-uglify'),
+	less = require('gulp-less'),
+	uncss = require('gulp-uncss'),
+	concat = require('gulp-concat'),
 
-var tasks = ['scripts', 'index-minify', 'styles', 'bower-to-dist', 'images'];
+	tasks = ['less', 'scripts', 'index-minify', 'bower-to-dist', 'images'];
 
-gulp.task('styles', function() {
-	gulp.src('src/css/*.css')
-		.pipe(minifyCSS())
-		.pipe(gulp.dest('dist/css'));
+gulp.task('less', function () {
+	gulp.src(['less/custom-styles.less', 'css/stylesheet.css'])
+    .pipe(less())
+    .pipe(uncss({
+		html: ['index.html']
+	}))
+	.pipe(concat('styles.css'))
+    .pipe(minifyCSS())
+	.pipe(gulp.dest('../dist/css'));
 });
 
 gulp.task('scripts', function() {
-	gulp.src('src/js/*.js')
+	gulp.src('js/*.js')
 		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'));
+		.pipe(gulp.dest('../dist/js'));
 });
 
 gulp.task('images', function() {
-	gulp.src('src/images/*')
-		.pipe(gulp.dest('dist/images'));
+	gulp.src('images/*')
+		.pipe(gulp.dest('../dist/images'));
 });
 
 gulp.task('index-minify', function() {
-	gulp.src('src/*.html')
+	gulp.src('*.html')
 		.pipe(htmlmin({collapseWhitespace: true}))
 		.pipe(htmlmin({removeComments: true}))
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('../dist'));
 });
 
 gulp.task('bower-to-dist', function() {
-	gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css'])
-		.pipe(gulp.dest('dist/css'));
 	gulp.src(['bower_components/bootstrap/dist/js/bootstrap.min.js'])
-		.pipe(gulp.dest('dist/js'));	
+		.pipe(gulp.dest('../dist/js'));	
 	gulp.src(['bower_components/jquery/dist/jquery.min.js'])
-		.pipe(gulp.dest('dist/js'));
+		.pipe(gulp.dest('../dist/js'));
 	gulp.src(['bower_components/angularjs/angular.min.js'])
-		.pipe(gulp.dest('dist/js'));
+		.pipe(gulp.dest('../dist/js'));
 });
 
 gulp.task('watch', function (){
-	gulp.watch(['src/css/*.css', 'src/*.html', 'src/images/*', 'src/js/*.js'], tasks);
-});
+	gulp.watch(['less/*.less', 'css/*.css', '*.html', 'images/*', 'js/*.js'], tasks);
+})
 
 tasks.push('watch');
 
